b71f4e82ef5e6da358062ac06a60e2d257cca2dd
Thibaut Degbetchi <Thibautdegbetchi@gmail.com>
2024-11-19 16:26:21 +0100

    update : profile_screen.dart ui

--- lib/presentation/profile_screen/controller/profile_controller.dart
+++ lib/presentation/profile_screen/controller/profile_controller.dart
@@ -1,3 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ProfileController extends GetxController {}
+import '../../../core/utils/color_constant.dart';
+import '../../../routes/app_routes.dart';
+
+class ProfileController extends GetxController {
+  List<Icon> icons = [
+    Icon(
+      Icons.history,
+      color: ColorConstant.orange,
+    ),
+    Icon(
+      Icons.add_card,
+      color: ColorConstant.orange,
+    ),
+    Icon(
+      Icons.location_on,
+      color: ColorConstant.orange,
+    ),
+    Icon(
+      Icons.people_alt_sharp,
+      color: ColorConstant.orange,
+    ),
+  ];
+
+  List<String> titles = [
+    'lbl_order_history',
+    'lbl_payment_methods',
+    'msg_delivery_address',
+    'lbl_user_pref'
+  ];
+  List<String> subTitles = [
+    'msg_order_info',
+    'msg_payment',
+    'msg_delivery_location',
+    'msg_user_pref'
+  ];
+}
--- lib/presentation/profile_screen/profile_screen.dart
+++ lib/presentation/profile_screen/profile_screen.dart
@@ -4,6 +4,9 @@ import 'package:get/get.dart';
 import 'package:restaurant/presentation/profile_screen/controller/profile_controller.dart';
 
 import '../../core/utils/color_constant.dart';
+import '../../core/utils/size_utils.dart';
+import '../../routes/app_routes.dart';
+import '../../widgets/custom_button.dart';
 
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({super.key});
@@ -26,50 +29,121 @@ class ProfileScreen extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                SizedBox(
-                  height: 30,
-                  width: 35,
-                  // color: Colors.red,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: 0.7,
-                        right: 0.1,
-                        bottom: 0.3,
-                        child: IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.lock_outline,
-                              size: 20,
-                            )),
-                      ),
-                      Positioned(
-                          right: 3,
-                          // top: 1,
-                          child: Container(
-                            height: 15,
-                            width: 15,
-                            padding: const EdgeInsets.only(left: 5),
-                            decoration: BoxDecoration(
-                                color: ColorConstant.deep,
-                                borderRadius: const BorderRadius.all(
-                                    Radius.circular(10))),
-                            child: Text(
-                              "msg_nb".tr,
-                              style: TextStyle(
-                                  color: ColorConstant.whiteA700, fontSize: 10),
-                            ),
-                          ))
-                    ],
-                  ),
-                ),
                 IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
               ],
             )
           ],
         ),
-        body: Stack(
-          children: [Container()],
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/images/images_coffee.png'),
+              radius: 50,
+            ),
+            Text(
+              'Thibaut Degbetchi',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              'thibautdegbetchi@gmail.com',
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 11,
+                color: ColorConstant.grey,
+              ),
+            ),
+            Container(
+                height: size.height * 0.054,
+                width: size.width * 0.4,
+                margin: getMarginOrPadding(all: 15),
+                child: CustomButton(
+                  shape: ButtonShape.RoundedBorder15,
+                  text: 'lbl_edit_profil'.tr,
+                  onTap: () {},
+                )),
+            SizedBox(
+              height: size.height * 0.5,
+              width: size.width,
+              child: ListView.builder(
+                  itemCount: 4,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                        onTap: () {
+                          switch (index) {
+                            case 0:
+                              Get.toNamed(AppRoutes.profileScreen);
+                              break;
+                            case 1:
+                              Get.toNamed(AppRoutes.profileScreen);
+                              break;
+                            case 2:
+                              Get.toNamed(AppRoutes.profileScreen);
+                              break;
+                            case 3:
+                              Get.toNamed(AppRoutes.profileScreen);
+                              break;
+                            default:
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              OptionWidget(
+                                controller.icons[index],
+                                controller.titles[index],
+                                controller.subTitles[index],
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios_sharp,
+                                color: ColorConstant.black,
+                              )
+                            ],
+                          ),
+                        ));
+                  }),
+            )
+          ],
         ));
   }
+}
+
+Widget OptionWidget(Icon icon1, String title, String subTitle) {
+  return SizedBox(
+    height: size.height * 0.1,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            icon1,
+            SizedBox(
+              width: size.width * 0.08,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title.tr,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                Text(
+                  subTitle.tr,
+                  style: TextStyle(fontSize: 11, color: ColorConstant.grey),
+                )
+              ],
+            ),
+          ],
+        )
+      ],
+    ),
+  );
 }
